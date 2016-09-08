af095cdb5a766cbd6e6038953fdad697770b64c2
Subramanian <vishwanath79@outlook.com>
2016-09-07 21:07:33 -0700

    errors

--- MiscScripts/4_Functions.py
+++ MiscScripts/4_Functions.py
@@ -1,5 +1,8 @@
 import csv
-def portfolio_cost(filename):
+def portfolio_cost(filename, *, errors='warn'):
+
+    if errors not in {'warn','silent','raise'}:
+        raise ValueError("error must be one of 'warn','silent','raise'")
     total = 0.0
     with open(filename, 'r') as f:
         rows = csv.reader(f)
@@ -11,11 +14,12 @@ def portfolio_cost(filename):
                 row[3] = float(row[3])
 
             except ValueError as err:
+                if errors == 'warn':
                 print("Row: ", rowno, 'Bad row: ', row)
                 print('Row: ', rowno, 'Reason:', err)
                 continue
             total += row[2] * row[3]
     return total
 
-total = portfolio_cost('portfolio.csv')
+total = portfolio_cost('portfolio.csv', errors='ignore')
 print('Total Cost: ' , total)
