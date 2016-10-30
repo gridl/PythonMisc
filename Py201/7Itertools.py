b1e90a456063599ef07413366cca9b3652ffe1e2
Subramanian <vishwanath79@outlook.com>
2016-10-29 22:04:53 -0700

    more iterators

--- Py201/7Itertools.py
+++ Py201/7Itertools.py
@@ -1,4 +1,5 @@
-from itertools import count, islice, cycle, repeat, accumulate, chain
+from itertools import count, islice, cycle, repeat, accumulate, chain,zip_longest, \
+    combinations, combinations_with_replacement, product
 for i in count(10):
     if i > 20:
         break
@@ -20,5 +21,18 @@ print(list(accumulate(range(10))))
 
 #flattening lists into one using chain
 
-my_list = list(chain(['foo','bar'], cmd, numbers))
-print(my_list)
+#my_list = list(chain(['foo','bar'], cmd, numbers))
+#print(my_list)
+
+for item in  zip_longest('ABCD', 'xy', fillvalue='BLANK'):
+    print(item)
+
+a = list(combinations('WXYZ', 2))
+print(a)
+
+for item in combinations_with_replacement('WXYZ', 2):
+    print(''.join(item))
+
+arrays = [(-1,1), (-3,3), (-5,5)]
+cp = list(product(*arrays))
+print(cp)
