3240a1357d289be3e43b55fb516a035a4dc76db8
Subramanian <vishwanath79@outlook.com>
2016-11-30 17:53:39 -0800

    collection

---
+++ 20Pythonlibs/collection.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict,defaultdict, namedtuple
+from string import ascii_lowercase
+
+print(OrderedDict(zip(ascii_lowercase,range(4))))
+
+# specify a default value for all new keys
+
+d = defaultdict(list)
+print(d['a'])
+
+A = namedtuple('A', 'count enabled color')
+tup = A(count=1, enabled=True, color = "red")
+print(tup.count)
+print(tup.color)
