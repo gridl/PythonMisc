23fb2f4feb2d4c728ff8a6d63ef48d293e160fcf
Subramanian <vishwanath79@outlook.com>
2016-12-06 20:53:39 -0800

    begins

--- 20Pythonlibs/argparsetest.py
+++ 20Pythonlibs/argparsetest.py
@@ -4,7 +4,7 @@ def main(a,b):
     """ SHort script to add two numbers"""
     return a + b
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="add two numbers")
     parser.add_argument('-a', help='First value', type=float,default=0)
     parser.add_argument('-b',help='second value',type=float,default=0)
