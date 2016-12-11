8a0bdb691319aa94a247715713372a3c8af64b64
Subramanian <vishwanath79@outlook.com>
2016-12-11 13:01:10 -0800

    add scheduler

--- SimpleCraigsList/SimpleCraigsList.py
+++ SimpleCraigsList/SimpleCraigsList.py
@@ -1,45 +1,65 @@
-from bs4 import BeautifulSoup
-import requests
-import logging
+import smtplib
+import sys
+import time
 import colorlog
+import conf
+import requests
+import schedule
+from bs4 import BeautifulSoup
 
 site = 'http://portland.craigslist.org/search/muc?query=guitar'
 html = 'SimpleCraigsList/craigs.html'
 logger = colorlog.getLogger()
+sys.path.append('/Users/vsubr2/Projects/RedditBot/conf')
+allposts = []
 
-
-def fetch_search_results(query = None):
-    resp = requests.get(site,timeout=3)
+def fetch_search_results(query=None):
+    resp = requests.get(site, timeout=3)
     resp.raise_for_status()
     file = open("craigs.html", "w")
     file.write(resp.text)
     file.close()
-    #return resp.content,resp.encoding
+    # return resp.content,resp.encoding
 
-def parse_source(html, encoding='utf-8'):
-    soup = BeautifulSoup(open("craigs.html"),"lxml")
-    logger.info('Printing Soup')
-    #print(soup.prettify())
-    logger.info("Finding rows")
-    rows = soup.find('div', class_ ='content').find_all('p', 'row')
-    print(rows)
-    for row in rows:
-        print(row)
-    #print(soup.prettify())
-    logger.info("FInding titles and links")
+
+def parse_source():
+    soup = BeautifulSoup(open("craigs.html"), "lxml")
+    # logger.info('Printing Soup')
+    # print(soup.prettify())
+    # logger.info("Finding rows")
+    print(soup.prettify())
+    logger.info("Finding titles and links")
     for post in soup.find_all('a', class_='result-title hdrlnk'):
-        print(post.string,'http://portland.craigslist.org/'+post['href'])
+        posts = post.string, 'http://portland.craigslist.org/' + post['href']
+        allposts.append(str(posts))
+    #return '\n'.join(allposts).encode('utf-8')
+
+    print('\n'.join(allposts))
 
 
+def send_mail():
+    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(conf.send, conf.key)
+    msg = parse_source()
+    server.sendmail(conf.send, conf.rec, msg)
+    logger.info("Sending mail")
+    server.quit()
+
+
+def by_schedule():
+    schedule.every().day.at("12:44").do(send_mail)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == '__main__':
     logger.setLevel(colorlog.colorlog.logging.INFO)
     handler = colorlog.StreamHandler()
     handler.setFormatter(colorlog.ColoredFormatter())
     logger.addHandler(handler)
-
-    #fetch_search_results()
-    parse_source(html)
-
-
+    # fetch_search_results()
+    parse_source()
+    #by_schedule()
 
