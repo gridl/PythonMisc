6ca443071854e72014f0ebfbdf485db0bc648c8f
Subramanian <vishwanath79@outlook.com>
2017-01-29 10:58:19 -0800

    explore flask

---
+++ ExploreFlask/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+APP_CONFIG_FILE = '~/Projects/PythonMisc/ExploreFlask/myapp/config/production.py'
+python run.py
+
+# start.sh is unique to each environment and should be left out of version control.
