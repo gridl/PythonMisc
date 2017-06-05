530fc7f1abaab5f549925a0a4590af686526fbfe
Subramanian <vishwanath79@outlook.com>
2017-06-05 14:47:35 -0700

    urlopen

---
+++ Python3StandardLib/Networking/urllib_request_urlopen.py
@@ -0,0 +1,18 @@
+from urllib import request
+
+response = request.urlopen('http://www.reddit.com')
+print('RESPONSE:',response)
+print('URL :', response.geturl())
+
+headers = response.info()
+print('DATE:', headers['date'])
+print('HEADERS:')
+print('-------')
+print(headers)
+
+
+data = response.read().decode('utf-8')
+print('LENGTH: ', len(data))
+print('DATA: ')
+print('------------')
+#print(data)
