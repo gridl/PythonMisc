42a0da84905eddc9718f6dab2c2c18152b3f9f03
Subramanian <vishwanath79@outlook.com>
2017-10-02 12:29:39 -0700

    todo

--- swagger/flasrestplusexample.py
+++ swagger/flasrestplusexample.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Resource, Api
 
 app = Flask(__name__)
@@ -8,6 +8,21 @@ api = Api(app)
 class Test(Resource):
     def get(self):
         return {"what the": 'hell'}
+
+todos = {}
+
+@api.route('/<string:todo_id>')
+class ToDoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=8001)
