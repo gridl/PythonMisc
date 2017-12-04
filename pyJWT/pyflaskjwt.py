c0b0efda77a815332d3d819ff041ab026544c1e9
Subramanian <vishwanath79@outlook.com>
2017-12-03 18:11:55 -0800

    get auth token

--- pyJWT/pyflaskjwt.py
+++ pyJWT/pyflaskjwt.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'super-secret'
 api = Api(app,prefix='/api/v1')
 
-class PrivateResource(Resource):
-    def get(self):
-        return {"meaning of life": 42}
+# class PrivateResource(Resource):
+#     def get(self):
+#         return {"meaning of life": 42}
 
 
-api.add_resource(PrivateResource, '/private')
+#api.add_resource(PrivateResource, '/private')
 
 USER_DATA = {
     "vish":"abc123"
 
 }
+
+# construct the jwt instance
+
 
 class User(object):
     def __init__(self,id):
@@ -29,10 +35,29 @@ def verify(username, password):
     if USER_DATA.get(username) == password:
         return User(id=123)
 
+def identity(payload):
+    user_id = payload['identity']
+    return {"user_id":user_id}
+# the identity function will recieve the decoded JWT
 
+jwt = JWT(app, verify, identity)
+
+class PrivateResource(Resource):
+    @jwt_required()
+    def get(self):
+        return {"meaning of life": 42}
+
+api.add_resource(PrivateResource,'/private')
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+    # we pass the flask app instance, authentication function and identity function to the JWT class
+
     # http://127.0.0.1:5000/api/v1/private
 
 
+# to get an auth token, send a POST request to /auth with a JSON payload with username and password
 
+# curl -H "Content-Type: application/json" -X POST -d '{"username":"vish","password":"abc123"}' http://localhost:5000/auth
+
+# curl -X GET http://localhost:5000/api/v1/private -H "Authorization: JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MTIzNTM1NTksImlhdCI6MTUxMjM1MzI1OSwiaWRlbnRpdHkiOjEyMywibmJmIjoxNTEyMzUzMjU5fQ.hYibp1qr1kb0HxKSqIzrZBK8P2VgwD3h38AbyZJgERA"
